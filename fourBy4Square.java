c5b5047dc84a6b2bf2224305411b2d89ac2fc6d3
RISHAV KUMAR MISHRA <52624501+rishavmishra2000@users.noreply.github.com>
2022-08-31 16:19:01 +0530

    Update fourBy4Square.java

--- fourBy4Square.java
+++ fourBy4Square.java
@@ -1,3 +1,6 @@
+// Must watch --> https://www.youtube.com/shorts/CGUq_YAjpkg
+// Like, share and subscribe my channel
+
 public class fourBy4Square {
 	public static void main(String[] args) {
 		for(int i = 1 ; i <= 4 ; i++){
