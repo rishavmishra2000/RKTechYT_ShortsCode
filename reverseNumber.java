068872d7dad36ea747d26ae7875fb41366d75910
Rishav Kumar Mishra <rishavmishra2000@gmail.com>
2022-08-31 16:09:19 +0530

    Reverse the number source code

---
+++ reverseNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num, revNum = 0;
+		num = sc.nextInt();
+		
+		System.out.println("Original Number: " + num);
+		
+		while(num != 0){
+			int temp = num % 10;
+			revNum = revNum * 10 + temp;
+			num = num / 10;
+		}
+		
+		System.out.println("Reverse Number: " + revNum);
+	}
+}
