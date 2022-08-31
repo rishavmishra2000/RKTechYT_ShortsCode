afbd5984b2baaf784e65af9f63085f20e1b15a66
RISHAV KUMAR MISHRA <52624501+rishavmishra2000@users.noreply.github.com>
2022-08-31 15:56:28 +0530

    Update QuotientRemainder.java

--- QuotientRemainder.java
+++ QuotientRemainder.java
@@ -1,3 +1,6 @@
+// Must Watch --> https://www.youtube.com/shorts/LyK_OXgrSOc
+// Like, share and subscribe my channel
+
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
