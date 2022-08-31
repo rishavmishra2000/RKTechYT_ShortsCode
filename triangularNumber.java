8ab355b9c6f453c79bed1b86412f4f7aad307a19
Rishav Kumar Mishra <rishavmishra2000@gmail.com>
2022-08-31 16:10:31 +0530

    Printing Triangular number, source code

---
+++ triangularNumber.java
@@ -0,0 +1,11 @@
+public class triangularNumber{
+      public static void main(String []args){
+            int x = 1;
+            int val = 0;
+            
+            for ( ; x <= 10 ; x++){
+                  val = val + x;
+                  System.out.println(val);
+            }
+      }
+}
