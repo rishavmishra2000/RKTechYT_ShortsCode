aba2391f801dd81d3bb0a050a0be2ee063e8169d
Rishav Kumar Mishra <rishavmishra2000@gmail.com>
2022-08-31 16:04:47 +0530

    find factorial of a number

---
+++ factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a number: ");
+		int num = sc.nextInt();
+		int fact = 1;
+		
+		if(num == 0){
+			System.out.println(num + "!" + " is " + fact);
+		} else {
+			for(int i = 1 ; i <= num ; i++){
+				fact = fact * i;
+			}
+			System.out.println(num + "!" + " is " + fact);
+		}
+	}
+}
