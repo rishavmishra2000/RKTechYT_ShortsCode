d99dce19f7876165e5772ee9f4b1e9571e77fb4e
RISHAV KUMAR MISHRA <52624501+rishavmishra2000@users.noreply.github.com>
2022-08-31 16:28:59 +0530

    Update triangularNumber.java

--- triangularNumber.java
+++ triangularNumber.java
@@ -1,3 +1,6 @@
+// Must Watch --> https://www.youtube.com/shorts/Y7VVc67ycrs
+// Like, share and subscribe my channel
+
 public class triangularNumber{
       public static void main(String []args){
             int x = 1;
