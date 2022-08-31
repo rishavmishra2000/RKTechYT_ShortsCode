a51a3c09e043154593fe120b99c58ec1b032ca72
Rishav Kumar Mishra <rishavmishra2000@gmail.com>
2022-08-31 16:08:41 +0530

    Swap the number, A <=> B

---
+++ numberSwap.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, temp;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println("Before swap: " + a + " ," + b);
+		
+		temp = a;
+		a = b;
+		b = temp;
+		
+		System.out.println("After swap: " + a + " ," + b);
+	}
+}
