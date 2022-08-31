6273185b18284da9259567600c7a00e080248094
Rishav Kumar Mishra <rishavmishra2000@gmail.com>
2022-08-31 15:47:33 +0530

    Finding Quotient and Remainder Source Code

---
+++ QuotientRemainder.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int dividend = sc.nextInt();
+		int divisor = sc.nextInt();
+		
+		int quotient = dividend / divisor;
+		int remainder = dividend % divisor;
+		System.out.println("Quotient= " + quotient);
+		System.out.println("Remainder= " + remainder);
+		
+	}
+}
