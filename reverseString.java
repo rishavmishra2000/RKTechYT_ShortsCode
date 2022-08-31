5edf3a3cb8f38e71e032af5f81202bd5e396a75f
RISHAV KUMAR MISHRA <52624501+rishavmishra2000@users.noreply.github.com>
2022-08-31 16:00:39 +0530

    Update reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String revStr = "";
 		
+		for(int i = str.length() - 1; i >= 0 ; i--){
+			revStr = revStr + str.charAt(i);
+		}
+		
+		System.out.println("Original String= " + str);
+		System.out.println("Reversed String= " + revStr);
 	}
 }
